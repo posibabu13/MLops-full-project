dc43585350b0d51f7daa0cc5c9b6ee4111437d04
babu <posibabu.sankarapu@gmail.com>
2025-10-02 11:50:27 +0530

    Project structure ready and venv established

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="posibabu",
+    author_email="posibabu.sankarapu@gmail.com",
+    packages=find_packages()
+)
